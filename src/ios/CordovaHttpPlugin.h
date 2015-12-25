c8e7223e1ebbaa0a66998681d835fd6f5dbbd09c
Guy S <guy@hostrick.co.il>
2015-12-25 17:11:54 +0200

    Update CordovaHttpPlugin.h

--- src/ios/CordovaHttpPlugin.h
+++ src/ios/CordovaHttpPlugin.h
@@ -11,6 +11,7 @@
 - (void)acceptAllCerts:(CDVInvokedUrlCommand*)command;
 - (void)post:(CDVInvokedUrlCommand*)command;
 - (void)get:(CDVInvokedUrlCommand*)command;
+- (void)put:(CDVInvokedUrlCommand*)command;
 - (void)uploadFile:(CDVInvokedUrlCommand*)command;
 - (void)downloadFile:(CDVInvokedUrlCommand*)command;
 
